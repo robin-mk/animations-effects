32ea548fe4517745f48331e9195f4b3c15100f72
robin-mk <robin.meltingk@gmail.com>
2021-12-12 14:10:14 +0100

    speed up on scroll working

--- speedup_onscroll/main.js
+++ speedup_onscroll/main.js
@@ -2,103 +2,47 @@
 
 let texte = document.getElementById("loading");
 var time = 0;
-var interval = setInterval(timeCount, 50);
+var interval = setInterval(timeCount, 10);
 
 function timeCount() {
-  time += 1;
+  time += 1 + Math.abs(delta);
   // console.log(time)
   texte.style.transform = "translateX(" + time + "px)";
-  if (time > 1500) {
+  texte.style.transition = "all 0.5s ease-out";
+  if (time > 1200) {
     time = 0;
   }
 }
 
 
-// WITH HOVER
-// texte.addEventListener("mouseover", function( event ) {
-//   clearInterval(interval);
-//   interval = setInterval(timeCount, 1);
-// })
+var delta;
 
-// texte.addEventListener("mouseout", function( event ) {
-//   clearInterval(interval);
-//   interval = setInterval(timeCount, 10);
-// })
+var checkScrollSpeed = (function(settings){
+  settings = settings || {};
 
+  var lastPos, newPos, timer, 
+      delay = settings.delay || 50; // in "ms" (higher means lower fidelity )
 
-// WITH SCROLL// Setup isScrolling variable
-var isScrolling;
-var animOrNot = false;
+  function clear() {
+    lastPos = null;
+    delta = 0;
+  }
 
-window.addEventListener('scroll', function () {
+  clear();
 
-  // Clear our timeout throughout the scroll
-  window.clearTimeout(isScrolling);
-  animOrNot = true;
-
-  // Set a timeout to run after scrolling ends
-  isScrolling = setTimeout(function () {
-
-    animOrNot = false;
-
-    if (animOrNot) {
-      clearInterval(interval);
-    } else {
-      clearInterval(interval);
-      interval = setInterval(timeCount, 50);
+  return function(){
+    newPos = window.scrollY;
+    if ( lastPos != null ){ // && newPos < maxScroll 
+      delta = newPos -  lastPos;
     }
+    lastPos = newPos;
+    clearTimeout(timer);
+    timer = setTimeout(clear, delay);
+    return delta;
+  };
+})();
 
-  }, 200);
-
-  animationScrolling();
-
-}, false)
-
-function animationScrolling(value) {
-
-  clearInterval(interval);
-  interval = setInterval(timeCount, 10);
-  // var scroll = window.scrollY;
-  // if (scroll >= 0) {
-  // } else {
-  // }
-}
-
-// Accelerator
-var x = 0,
-  int = 200,
-  valueStop = 200;
-
-function increase() {
-  setTimeout(function () {
-    // console.log(x++);
-    console.log(int);
-    if (int <= (0.2 * valueStop)) {
-      int += 10;
-      decrease(int);
-      return false;
-      if (int == 1000) {
-      }
-    } else {
-      int -= 10;
-    }
-    increase();
-  }, int);
-}
-
-increase();
-
-function decrease() {
-  setTimeout(function () {
-    // console.log(x++);
-    console.log(int);
-    if (int <= (valueStop)) {
-      int += 10;
-      decrease(int);
-      return false;
-    } else {
-      return false;
-    }
-    decrease();
-  }, int);
-}
+// listen to "scroll" event
+window.onscroll = function(){
+console.log( checkScrollSpeed() );
+};
