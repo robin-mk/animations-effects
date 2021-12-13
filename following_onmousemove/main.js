b056202df23a5e77935828c42f9eae11bf8ee8d2
robin-mk <robin.meltingk@gmail.com>
2021-12-13 14:57:29 +0100

    ball follow mouse

--- following_onmousemove/main.js
+++ following_onmousemove/main.js
@@ -1,11 +1,15 @@
 const ball1 = document.getElementById("ball1");
 const background = document.getElementById("background");
 
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+ball1.style.transition = "all 1s ease-out";
 
 background.addEventListener("mousemove", function myScript(event) {
-    var x = event.clientX;     // Get the mouse horizontal coordinate
-    var y = event.clientY;     // Get the mouse vertical coordinate
-    console.log(x,y)
-    ball1.style.transform = "translate(" + x + offsetXBall1 + "px," + y + offsetYBall1 + "px)";
-} );
+    let x = event.clientX - width / 2;     // Get the mouse horizontal coordinate centered
+    let y = event.clientY - height / 2;     // Get the mouse vertical coordinate centered
+
+    ball1.style.transform = "translate(" + x * 1.3 + "px," + y * 1.3 + "px)";
+});
 
