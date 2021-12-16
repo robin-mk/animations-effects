ff5b2e90cc8178ebdfc898d5ae4c061685697984
robin-mk <robin.meltingk@gmail.com>
2021-12-16 18:01:29 +0100

    new method

--- speedup_onscroll/main.js
+++ speedup_onscroll/main.js
@@ -8,18 +8,88 @@ let time2 = 0;
 let intervalTimer = 5; // precision de l'interval et donc de la transform
 let interval = setInterval(timeCount, intervalTimer);
 let initialSpeed = 1; // 0 ou 1
+let speed = 0.2;
 
 let texteWidth = texte.getBoundingClientRect().width;
 let texte2Width = texte2.getBoundingClientRect().width;
 
 
-let speed = 1;
+// VERSION CREATE DIV
+// ****************
+
+let sentenceText = "MY NAME IS ROBIN";
+let separatorText = "|";
+const sentencesContainer = document.querySelector('#sentencesContainer');
+const sentence = document.createElement('div');
+sentence.textContent = sentenceText;
+sentence.className = "sentence itemToTranslate";
+const separator = document.createElement('div');
+separator.textContent = separatorText;
+separator.className = "separator itemToTranslate";
+
+initiate();
+
+async function initiate() {
+  await createDOMElements();
+  setInterval(timeCount2, intervalTimer);
+}
+
+async function createDOMElements() {
+  sentencesContainer.appendChild(sentence);
+  sentencesContainer.appendChild(separator);
+
+  let sentenceWidth = sentence.getBoundingClientRect().width;
+  let separatorWidth = separator.getBoundingClientRect().width;
+
+  console.log(sentenceWidth, separatorWidth, window.innerWidth)
+  console.log(Math.ceil(window.innerWidth / (sentenceWidth + separatorWidth)))
+
+  // creation d'entite tant que l'ecran n'est pas rempli
+
+  for (let i = 0; i < Math.ceil(window.innerWidth / (sentenceWidth + separatorWidth)); i++) {
+    sentencesContainer.appendChild(sentence.cloneNode(true));
+    sentencesContainer.appendChild(separator.cloneNode(true));
+  }
+
+  // return (sentencesContainer);
+
+}
+
+// ANIMATION PART
+function timeCount2() {
+  time += initialSpeed + delta;
+  let itemToTranslate = document.querySelectorAll(".itemToTranslate");
+  let i = 0;
+
+  // IL FAUT CREER UN ARRAY AVEC UN TIME DIFFERENT POUR CHAQUE ITEM
+
+  itemToTranslate.forEach(function (item) {
+    i++;
+    item.style.transform = "translateX(" + - time * i * speed + "px)";
+  });
+
+  if (time > 20 + texteWidth / speed) { // text1 plus à l'écran à gauche : invisible
+    texte.style.opacity = 0;
+    time = -texteWidth / speed;
+  }
+  if (time > -texteWidth / speed) { // text1 bien arrivé à droite : passer visible
+    texte.style.opacity = 1;
+  }
+  if (time < - texteWidth / speed) { // si on revient en arrière alors que le texte est déplacé à droite
+    time = 20 + texteWidth / speed;
+  }
+}
+
+// sentencesContainer.appendChild(sentence);
+// ****************
+
+
 
 // ANIMATION PART
 function timeCount() {
   time += initialSpeed + delta;
   time2 += initialSpeed + delta;
-  console.log(time2, texte2Width)
+  // console.log(time2, texte2Width)
   if (time > 20 + texteWidth / speed) { // text1 plus à l'écran à gauche : invisible
     texte.style.opacity = 0;
     time = -texteWidth / speed;
@@ -40,12 +110,12 @@ function timeCount() {
   if (time2 < 0) { // si on revient en arrière alors que le texte2 est déplacé à droite
     time2 = texte2Width / speed + texteWidth / speed;
   }
-  texte.style.transform = "translateX(" + - time * speed + "px)";
+  // texte.style.transform = "translateX(" + - time * speed + "px)";
   // texte.style.transition = "transform 0.4s ease-out";
-  texte2.style.transform = "translateX(" + - time2 * speed + "px)";
+  // texte2.style.transform = "translateX(" + - time2 * speed + "px)";
   // texte2.style.transition = "transform 0.4s ease-out";
-  rotator.style.transform = "rotate(" + time / 5 + "deg)";
-  rotator.style.transition = "transform 0.4s ease-out";
+  // rotator.style.transform = "rotate(" + time / 5 + "deg)";
+  // rotator.style.transition = "transform 0.4s ease-out";
 }
 
 
