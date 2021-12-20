03a05e632d69fdb5e54b8c6d95e50bd31a310843
robin-mk <robin.meltingk@gmail.com>
2021-12-20 15:48:35 +0100

    update scroll

--- speedup_onscroll/main.js
+++ speedup_onscroll/main.js
@@ -1,37 +1,27 @@
-
-
-let texte = document.getElementById("loading");
-let texte2 = document.getElementById("loading2");
 let rotator = document.getElementById("rotator");
-let time = 0;
-let time2 = 0;
 let intervalTimer = 5; // precision de l'interval et donc de la transform
-let interval = setInterval(timeCount, intervalTimer);
 let initialSpeed = 1; // 0 ou 1
 let speed = 1;
 let sentenceWidth;
 let separatorWidth;
 let indiceItem = 0;
 let delta = 0;
-let timeArray;
+let sentencesTimeArray;
+let separatorsTimeArray;
 let itemToTranslate;
-
-let texteWidth = texte.getBoundingClientRect().width;
-let texte2Width = texte2.getBoundingClientRect().width;
-
-
-// VERSION CREATE DIV
-// ****************
 
 let sentenceText = "MY NAME IS ROBIN";
 let separatorText = "|";
 const sentencesContainer = document.querySelector('#sentencesContainer');
 const sentence = document.createElement('div');
 sentence.textContent = sentenceText;
-sentence.className = "sentence itemToTranslate";
+sentence.className = "sentenceInfinity itemToTranslate";
 const separator = document.createElement('div');
 separator.textContent = separatorText;
-separator.className = "separator itemToTranslate";
+separator.className = "separatorInfinity itemToTranslate";
+
+// array length
+let lengthArray;
 
 initiate();
 
@@ -46,34 +36,60 @@ async function createDOMElements() {
 
   sentenceWidth = sentence.getBoundingClientRect().width;
   separatorWidth = separator.getBoundingClientRect().width;
+  lengthArray = Math.ceil(window.innerWidth / (sentenceWidth + separatorWidth)) + 1;
 
   // initialise une array de 0 de la bonne taille : stocke tous les temps pour chaque transform
-  timeArray = new Array(2 * Math.ceil(window.innerWidth / (sentenceWidth + separatorWidth))).fill(0);
+  sentencesTimeArray = new Array(lengthArray).fill(0);
+  separatorsTimeArray = new Array(lengthArray).fill(0);
 
   // creation d'entite tant que l'ecran n'est pas rempli
-  for (let i = 0; i < Math.ceil(window.innerWidth / (sentenceWidth + separatorWidth)); i++) {
+  for (let i = 0; i < lengthArray; i++) {
     sentencesContainer.appendChild(sentence.cloneNode(true));
     sentencesContainer.appendChild(separator.cloneNode(true));
   }
-  
+
   itemToTranslate = document.querySelectorAll(".itemToTranslate");
+  sentenceInfinity = document.querySelectorAll(".sentenceInfinity");
+  separatorInfinity = document.querySelectorAll(".separatorInfinity");
+
 
 }
 
 // ANIMATION PART
 function timeCount2() {
   // pour les phrases
-  for (let i = 0; i < 2*Math.ceil(window.innerWidth / (sentenceWidth + separatorWidth)); i+=2) {
-    timeArray[i] += initialSpeed + delta;
-    if (timeArray[i] > (sentenceWidth + separatorWidth) * (i)) {
-      timeArray[i] = -(window.innerWidth + sentenceWidth + separatorWidth);
+  for (let i = 0; i < lengthArray; i++) {
+    sentencesTimeArray[i] += initialSpeed + delta;
+    // replacement au debut si disparait a gauche
+    if (sentencesTimeArray[i] > (sentenceWidth * (i + 1) + separatorWidth * (i + 1))) {
+      sentencesTimeArray[i] -= lengthArray * sentenceWidth + lengthArray * separatorWidth;
+      
+  // text plus à l'écran à gauche : invisible
+      sentenceInfinity[i].style.opacity = 0;
+
+      // partie pour gerer la transition
+      // sentenceInfinity[i].style.transition = "transform 0s linear";
+      // sentenceInfinity[i].style.opacity = 0;
+      // setTimeout(() => {
+      //   sentenceInfinity[i].style.transition = "transform 0.2s ease-out";
+      //   sentenceInfinity[i].style.opacity = 1;
+      // },20)
     }
   }
   // pour les separateurs
-  for (let i = 1; i < 2*Math.ceil(window.innerWidth / (sentenceWidth + separatorWidth)); i+=2) {
-    timeArray[i] += initialSpeed + delta;
-    if (timeArray[i] > (sentenceWidth + separatorWidth) * (i-1)) {
-      timeArray[i] = -(window.innerWidth + sentenceWidth + separatorWidth);
+  for (let i = 0; i < lengthArray; i++) {
+    separatorsTimeArray[i] += initialSpeed + delta;
+    // replacement au debut si disparait a gauche
+    if (separatorsTimeArray[i] > (sentenceWidth * (i + 1) + separatorWidth * (i + 1))) {
+      separatorsTimeArray[i] -= lengthArray * sentenceWidth + lengthArray * separatorWidth;
+      
+      // partie pour gerer la transition
+      // separatorInfinity[i].style.transition = "transform 0s linear";
+      // separatorInfinity[i].style.opacity = 0;
+      // setTimeout(() => {
+      //   separatorInfinity[i].style.transition = "transform 0.2s ease-out";
+      //   separatorInfinity[i].style.opacity = 1;
+      // },20)
     }
   }
 
@@ -81,10 +97,19 @@ function timeCount2() {
   let indice = 0;
 
   itemToTranslate.forEach(function (item) {
-    item.style.transform = "translateX(" + - timeArray[indice] * speed + "px)";
+    if ((indice % 2) === 0) { // is even
+      item.style.transform = "translateX(" + - sentencesTimeArray[indice / 2] * speed + "px)";
+    }
+    if (indice % 2 === 1 && indice < 2 * lengthArray) { // is odd
+      item.style.transform = "translateX(" + - separatorsTimeArray[Math.floor(indice / 2)] * speed + "px)";
+    }
     indice++;
   });
 
+  
+
+// BONNE METHODE POUR GERER LA TRANSITION INVISIBLE
+
   // if (time > 20 + texteWidth / speed) { // text1 plus à l'écran à gauche : invisible
   //   texte.style.opacity = 0;
   //   time = -texteWidth / speed;
@@ -92,51 +117,14 @@ function timeCount2() {
   // if (time > -texteWidth / speed) { // text1 bien arrivé à droite : passer visible
   //   texte.style.opacity = 1;
   // }
+
+
+
+
   // if (time < - texteWidth / speed) { // si on revient en arrière alors que le texte est déplacé à droite
   //   time = 20 + texteWidth / speed;
   // }
 }
-
-// sentencesContainer.appendChild(sentence);
-// ****************
-
-
-
-// ANIMATION PART
-function timeCount() {
-  // console.log("lala")
-  // time += initialSpeed + delta;
-  // time2 += initialSpeed + delta;
-  // // console.log(time2, texte2Width)
-  // if (time > 20 + texteWidth / speed) { // text1 plus à l'écran à gauche : invisible
-  //   texte.style.opacity = 0;
-  //   time = -texteWidth / speed;
-  // }
-  // if (time > -texteWidth / speed) { // text1 bien arrivé à droite : passer visible
-  //   texte.style.opacity = 1;
-  // }
-  // if (time < - texteWidth / speed) { // si on revient en arrière alors que le texte est déplacé à droite
-  //   time = 20 + texteWidth / speed;
-  // }
-  // if (time2 > 20 + texte2Width / speed + texteWidth / speed) { // text2 plus à l'écran à gauche : invisible
-  //   texte2.style.opacity = 0;
-  //   time2 = 0;
-  // }
-  // if (time > -texte2Width / speed) { // text2 bien arrivé à droite : passer visible
-  //   texte2.style.opacity = 1;
-  // }
-  // if (time2 < 0) { // si on revient en arrière alors que le texte2 est déplacé à droite
-  //   time2 = texte2Width / speed + texteWidth / speed;
-  // }
-  // texte.style.transform = "translateX(" + - time * speed + "px)";
-  // texte.style.transition = "transform 0.4s ease-out";
-  // texte2.style.transform = "translateX(" + - time2 * speed + "px)";
-  // texte2.style.transition = "transform 0.4s ease-out";
-  // rotator.style.transform = "rotate(" + time / 5 + "deg)";
-  // rotator.style.transition = "transform 0.4s ease-out";
-}
-
-
 
 // LISTEN SCROLL PART
 
